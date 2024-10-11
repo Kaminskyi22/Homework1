23ecce9a7ac549663a2d8d0ab9e5989f726db3d7
1 <1@DESKTOP-JGNM7MB>
2024-10-11 20:59:40 +0300

    Добавьте файлы проекта.

---
+++ Homework1.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+using namespace std;
+
+/*int main() {
+    cout << "Павло Зібров - \"Хрещатик\"\n\n";
+    cout << "Я і друзів розумів\n";
+    cout << "І ворогів умів прощати\n";
+    cout << "Бо хрещений батько мій\n";
+    cout << "То Хрещатик, то Хрещатик\n";
+    cout << "Я і друзів розумів\n";
+    cout << "І ворогів умів прощати\n";
+    cout << "Бо хрещений батько мій\n";
+    cout << "То Хрещатик, то Хрещатик!\n";
+    return 0;
+}
+*/
+
+/*int main() {
+    cout << "Every hunter wants\n";
+    cout << "\tto know where\n";
+    cout << "\t\tthe pheasant sits.\n";
+    cout << "\t\t\tRed\n";
+    cout << "\t\t\t\tOrange\n";
+    cout << "\t\t\t\t\tYellow\n";
+    cout << "\t\t\t\t\t\tGreen\n";
+    cout << "\t\t\t\t\t\t\tBlue\n";
+    cout << "\t\t\t\t\t\t\t\tIndigo\n";
+    cout << "\t\t\t\t\t\t\t\t\tViolet\n";
+    return 0;
+}
+*/
+int main() {
+    cout << "==============================\n";
+    cout << "\tОГОЛОШЕННЯ ПРО ПРОДАЖ\n";
+    cout << "==============================\n\n";
+
+    cout << "Продається: Велосипед\n";
+    cout << "Стан: Відмінний\n";
+    cout << "Колір: Чорний\n";
+    cout << "Ціна: 5000 грн\n\n";
+
+    cout << "Контактна інформація:\n";
+    cout << "\tІм'я: Влад\n";
+    cout << "\tТелефон: +38 (098) 995-33-02\n";
+    cout << "\tМісто: Рівне\n\n";
+
+    cout << "==============================\n";
+    cout << "Дзвоніть або пишіть, буду радий відповісти на ваші питання!\n";
+    cout << "==============================\n";
+
+    return 0;
+}
